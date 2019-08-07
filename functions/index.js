5d85ef149c0a2a681df1f535c66db22f61d9aaee
Andrey Zaharchuk <zaharlistru@gmail.com>
2019-08-07 06:24:20 +0300

    fix

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,52 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const express = require('express')
+const cors = require('cors')
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp(functions.config().firebase)
 
-const db = admin.firestore();
-const app = express();
+const db = admin.firestore()
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
+// get all hot dogs
 app.get('/hot-dogs', async (request, response) => {
   try {
     let hotDogs = []
-    let hotDogsRef = db.collection('hotDogs');
-    let allHotDogs = await hotDogsRef.get();
-    allHotDogs.then(snapshot => {
-      snapshot.forEach(doc => {
-        hotDogs.push(doc.data())
-      });
+    let hotDogsRef = db.collection('hotDogs')
+    let allHotDogs = await hotDogsRef.get()
+    allHotDogs.forEach(doc => {
+      hotDogs.push({
+        bun: doc.data().bun,
+        sausage: doc.data().sausage,
+        additive: doc.data().additive,
+        mustard: doc.data().mustard,
+        ketchup: doc.data().ketchup
+      })
     })
-    response.status(200).json(hotDogs).end();
+    response.status(200).json(hotDogs)
   } catch (error) {
-    response.status(500).send(error).end();
+    response.status(500).send(error)
   }
 });
 
+// get one hot dog
+app.get('/hot-dog/:id', (request, response) => {
+  let hotDogId = request.params.id
+  if (!hotDogId) throw new Error('HotDog ID is required')
+  let hotDog = db.collection('hotDogs').doc(hotDogId).get().then(doc => {
+    if (!doc.exists) {
+      response.status(204).json({ message: 'no-contact' })
+    } else {
+      response.status(200).json(doc.data())
+    }
+  }).catch(err => {
+    response.status(500).send(err)
+  });
+});
+
+// create hot dog
 app.post('/hot-dog', async (request, response) => {
   try {
     let rrr = {
@@ -39,7 +60,7 @@ app.post('/hot-dog', async (request, response) => {
       "ketchup": false,
       "mustard": false
     }
-    let { additive, sausage, bun, ketchup, mustard } = rrr;
+    let { additive, sausage, bun, ketchup, mustard } = rrr
     let data = {
       additive,
       sausage,
@@ -47,11 +68,11 @@ app.post('/hot-dog', async (request, response) => {
       ketchup,
       mustard
     }
-    await db.collection('hotDogs').add(data);
-    response.status(200).json({message: 'added'}).end();
+    await db.collection('hotDogs').add(data)
+    response.status(200).json({message: 'added'})
   } catch(error){
-    response.status(500).send(error).end();
+    response.status(500).send(error)
   }
-});
+})
 
 exports.api = functions.https.onRequest(app)
